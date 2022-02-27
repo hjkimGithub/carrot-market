437e1cdc28aa13c3dcc086e8fd0054b2b9efa4b4
hjkim <rlaguswo3408@gmail.com>
2022-02-27 09:57:28 +0900

    apply sendgrid

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import withHandler, {ResponseType} from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 const twilioClient = twilio(process.env.TWILIO_ACCSID, process.env.TWILIO_TOKEN);
 
@@ -74,6 +77,15 @@ async function handler(
             body: `Your login token: ${payload}.`,
         })
         console.log(message);
+    } else if(email) {
+        const email = await mail.send({
+            from: "rlaguswo3408@gmail.com",
+            to: "drbird1@naver.com",
+            subject: "Verify your email",
+            text: `Your token is ${payload}`,
+            html: `<strong> Your token is ${payload}</strong>`,
+        })
+        console.log(email);
     }
     return res.json({
         ok: true,
