95806ada9f3060bba19a8d2c87e74966d6fc62c8
hjkim <rlaguswo3408@gmail.com>
2022-03-01 11:23:59 +0900

    useUser + swr swr POC

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,20 +1,27 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function useUser() {
-    const [user, setUser] = useState();
+    const {data, error} =useSWR("/api/users/me");
+    // const [user, setUser] = useState();
     const router = useRouter();
+    // useEffect(() => {
+    //     fetch("/api/users/me")
+    //         .then(response => response.json())
+    //         .then((data) => {
+    //             if(!data.ok) {
+    //                 // return router.push("/enter");
+    //                 return router.replace("/enter");
+    //                 // replace <-> push: replace do not remain history
+    //             }
+    //             setUser(data.profile);
+    //         });
+    // }, [router]);
     useEffect(() => {
-        fetch("/api/users/me")
-            .then(response => response.json())
-            .then((data) => {
-                if(!data.ok) {
-                    // return router.push("/enter");
-                    return router.replace("/enter");
-                    // replace <-> push: replace do not remain history
-                }
-                setUser(data.profile);
-            });
-    }, [router]);
-    return user;
+        if(data && !data.ok) {
+            router.replace("/enter"); 
+        }
+    }, [data, router]);
+    return {user:data?.profile, isLoading: !data && !error};
 }
