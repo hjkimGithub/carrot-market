80b060633a19757044d20731972fa7bfab0aa743
hjkim <rlaguswo3408@gmail.com>
2022-03-05 21:14:10 +0900

    map, key properror fix

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -140,14 +140,14 @@ const CommunityPostDetail: NextPage = () => {
           </div>
         </div>
         <div key="answersforQuestion" className="px-4 my-5 space-y-5">
-          {data?.post?.answers?.map((answer) => <div className="flex items-start space-x-3">
-            <div key={answer.id} className="w-8 h-8 bg-slate-200 rounded-full" />
+          {data?.post?.answers?.map((answer) => <div key={answer.id} className="flex items-start space-x-3">
+            <div className="w-8 h-8 bg-slate-200 rounded-full" />
               <div>
                 <span className="text-sm block font-medium text-gray-700"> {" "}
                   {answer.user.name}
                 </span>
                 <span className="text-xs text-gray-500 block ">{answer.createdAt}</span>
-                <p className="text-gray-700 mt-2"> {answer.answer} </p>
+                <p key={answer.answer} className="text-gray-700 mt-2"> {answer.answer} </p>
               </div>
           </div>)}
         </div>
