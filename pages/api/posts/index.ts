2f25fa565d875e6c850b74d2bd41066239225569
hjkim <rlaguswo3408@gmail.com>
2022-03-05 22:12:57 +0900

    geolocation null exception

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,8 +7,11 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const {body: { question, latitude, longitude }, session: { user },} = req;
   if(req.method === "POST") {
+    const {
+      body: { question, latitude, longitude }, 
+      session: { user },
+    } = req;
     const post = await client.post.create({
       data: {
         question,
@@ -27,6 +30,11 @@ async function handler(
     });
   }
   if(req.method === "GET") {
+    const {
+      query: {latitude, longitude}
+    } = req;
+    const parsedLatitude = parseFloat(latitude.toString());
+    const parsedLongitude = parseFloat(longitude.toString());
     const posts = await client.post.findMany({
       include: {
         user: {
@@ -43,6 +51,16 @@ async function handler(
           },
         },
       },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLatitude + 0.01
+        },
+        longitude: {
+          gte: parsedLongitude - 0.01,
+          lte: parsedLongitude + 0.01
+        }
+      }
     });
     res.json({
       ok: true,
