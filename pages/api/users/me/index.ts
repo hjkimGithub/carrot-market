0dd76a3b3670588573de2883dd55377ce44dc12e
hjkim <rlaguswo3408@gmail.com>
2022-03-06 22:02:14 +0900

    useUser hook add profile resonse edit tsx: frontend

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -7,17 +7,21 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
-  });
-  res.json({
-    ok: true,
-    profile,
-  });
+  if(req.method === "GET"){
+    const profile = await client.user.findUnique({
+      where: { id: req.session.user?.id },
+    });
+    res.json({
+      ok: true,
+      profile,
+    });
+  } 
+  if(req.method === "POST"){ 
+  }
 }
 
 export default withApiSession(withHandler({
-  methods: ["GET"],
+  methods: ["GET", "POST"],
   handler,
   })
 );
