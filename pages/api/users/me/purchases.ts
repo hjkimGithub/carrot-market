742300a5d483e3b401908bc2a151c29b1113f608
hjkim <rlaguswo3408@gmail.com>
2022-03-06 20:08:34 +0900

    change locations favs, purchases, sales api

---
+++ pages/api/users/me/purchases.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+      session: {user},
+  } = req;
+  const purchases = await client.purchase.findMany({
+      where: {
+          userId: user?.id,
+      }
+  })
+  res.json({
+    ok: true,
+    purchases,
+  });
+}
+
+export default withApiSession(withHandler({
+  methods: ["GET"],
+  handler,
+  })
+);
