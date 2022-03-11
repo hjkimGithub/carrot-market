f8e0b37ecc9b9bbe73eee064333511055ef8e1b9
hjkim <rlaguswo3408@gmail.com>
2022-03-11 18:04:00 +0900

    implement middleware limit pathes

---
+++ pages/_middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if(req.ua?.isBot) {
+        return new Response("bot not allowed", {status: 403});
+    }
+    // console.log(req.cookies);
+    // console.log(req.url);
+    if(!req.url.includes("/api")) {
+        if(!req.url.includes("/enter") && !req.cookies.carrotsession) {
+            return NextResponse.redirect("/enter");
+        }
+    }
+}
