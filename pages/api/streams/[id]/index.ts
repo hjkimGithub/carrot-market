677ce96e40d875323a15a0bdcf2c6e1e806f823a
hjkim <rlaguswo3408@gmail.com>
2022-03-11 10:35:45 +0900

    securing stream

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
+import { userInfo } from "os";
 
 async function handler(
   req: NextApiRequest,
@@ -9,8 +10,39 @@ async function handler(
 ) {
   const {
     query: { id },
+    session: {user},
   } = req;
   const stream = await client.stream.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    // include: { // not secure, user can see important infos
+    select: {
+      // should specify things you want to show in api
+      id: true,
+      createdAt: true,
+      updatedAt: true,
+      name: true,
+      description: true,
+      price: true,
+      userId: true,
+      cloudflareId: true,
+      messages: {
+        select: {
+          id: true,
+          message: true,
+          user: {
+            select: {
+              avatar: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  const isOwner = stream?.userId === user?.id
+  const streamOwner = await client.stream.findUnique({
     where: {
       id: +id.toString(),
     },
@@ -29,7 +61,7 @@ async function handler(
       },
     },
   });
-  res.json({ ok: true, stream });
+  res.json({ ok: true, stream: isOwner ? streamOwner : stream });
 }
 
 export default withApiSession(
