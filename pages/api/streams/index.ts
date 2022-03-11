a4f3dcebfd79804ef3980691166609e537f203e7
hjkim <rlaguswo3408@gmail.com>
2022-03-11 10:02:31 +0900

    make livestream  backend, frontend

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -14,8 +14,28 @@ async function handler(
       },
   } = req;
   if(req.method === "POST") {
+    const {result: {
+      uid,
+      rtmps: {
+        url,
+        streamKey,
+      },
+    }} = await (await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.ACCOUNT_ID}/stream/live_inputs`, 
+      {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.STREAMS_TOKEN}`
+          },
+          body: `{"meta": {"name":"${name}"},"recording": { "mode": "automatic", "timeoutSeconds": 10}}`
+      }
+    )).json();
+    
     const stream = await client.stream.create({
       data: {
+        cloudflareId: uid,
+        cloudflareKey: streamKey,
+        cloudflareUrl: url,
         name,
         price,
         description,
