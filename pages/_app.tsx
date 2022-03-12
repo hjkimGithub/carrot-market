0c5cf3bb6d48b1709fdb8e43fdcd7007e2b7e08a
hjkim <rlaguswo3408@gmail.com>
2022-03-12 14:11:25 +0900

    lazyonload script test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import {SWRConfig} from "swr";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // console.log("APP IS RUNNING");
@@ -11,6 +12,24 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="w-full max-w-xl mx-auto">
         <Component {...pageProps} />
       </div>
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        // strategy='beforeInteractive'
+        // strategy='afterInteractive'
+        strategy="lazyOnload"
+      />
+      <Script src="https://connect.facebook.net/en_US/sdk.js" onLoad={() => {
+        window.fbAsyncInit = function () {
+          FB.init({
+            appId: "your-app-id",
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: "v13.0",
+          });
+        };
+      }}
+     
+      />
     </SWRConfig>
   );
 }
